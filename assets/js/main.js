da45e25fdf207f16e6cd0b8f5415042447116417
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-24 01:52:09 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+let num = 5;
+let anotherNum = 8;
+/* == */
+console.log(num == anotherNum);
+/* === */
+console.log(num === anotherNum);
+/* > */
+console.log(num > anotherNum);
+/* < */
+console.log(num < anotherNum);
+/* >= */
+console.log(num >= anotherNum);
+/* <= */
+console.log(num <= anotherNum);
+/* == "5" */
+console.log(num == "5");
+/* === 5 */
+console.log(num  === 5);
+/* === "5" */
+console.log(num === "5");
+/* != 8 */
+console.log(num != 8);
+/* != "5" */
+console.log(num != "5");
+/* !== "5" */
+console.log(num !== "5");
